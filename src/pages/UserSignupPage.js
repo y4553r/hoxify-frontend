d6347eef9dab741f120f491c28f403d6b3a61e3b
y4553r <abougueniyasser@outlook.com>
2020-01-06 10:48:52 +0100

    Add sign up form styling with bootstrap

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -29,42 +29,51 @@ export class UserSignupPage extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Sign Up</h1>
-        <div>
+      <div className="container">
+        <h1 className="text-center">Sign Up</h1>
+        <div className="col-12 mb-3">
+          <label>Display Name</label>
           <input
+            className="form-control"
             type="text"
             placeholder="Your display name"
             value={this.state.displayName}
             onChange={(e) => this.onChangeHandler('displayName', e)}
           />
         </div>
-        <div>
+        <div className="col-12 mb-3">
+          <label>Username</label>
           <input
+            className="form-control"
             type="text"
             placeholder="Your username"
             value={this.state.username}
             onChange={e => this.onChangeHandler('username', e)}
           />
         </div>
-        <div>
+        <div className="col-12 mb-3">
+        <label>Password</label>
           <input
+            className="form-control"
             type="password"
             placeholder='Your password'
             value={this.state.password}
             onChange={e => this.onChangeHandler('password', e)}
           />
         </div>
-        <div>
+        <div className="col-12 mb-3">
+        <label>Repeat password</label>
           <input
+            className="form-control"
             type="password"
             placeholder='Repeat your password'
             value={this.state.passwordRepeat}
             onChange={e => this.onChangeHandler('passwordRepeat', e)}
           />
         </div>
-        <div>
+        <div className="text-center">
           <button
+            className="btn btn-primary"
             onClick={this.onClickSignup}
           >Sign Up</button>
         </div>
