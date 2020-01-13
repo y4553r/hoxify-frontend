e166ac1abe4ae6a8427a0696abaf44035f5d9dd9
y4553r <abougueniyasser@outlook.com>
2020-01-13 14:48:13 +0100

    Add HomePage and UserPage and their test files

---
+++ src/pages/UserPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class UserPage extends Component {
+  render() {
+    return(
+      <div data-testid="userpage">
+
+      </div>
+    );
+  }
+}
+
+export default UserPage;
