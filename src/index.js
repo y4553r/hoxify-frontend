3ac1f1cbef88ab0f4fe2322391dbdffa9fb09acf
y4553r <abougueniyasser@outlook.com>
2020-01-14 09:53:15 +0100

    Install redux and create authReducer

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './containers/App';
+import authReducer from './redux/authReduxer';
+
+const store = createStore(authReducer);
 
 const app = (
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
